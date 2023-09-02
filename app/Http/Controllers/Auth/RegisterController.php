333fdafb0c4741559672dbd0d9da98c27a219e79
Asyifa Zahwa <azahwa201@gmail.com>
2023-09-02 12:18:43 +0700

    commit kedua

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,22 +3,19 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Models\User;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\RegisterRequest;
 
 class RegisterController extends Controller
 {
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Request $request)
+    public function __invoke(RegisterRequest $request)
     {
-        request()->validate([
-            'username' => ['alpha_num', 'required', 'min:3', 'max:25', 'unique:users,username'],
-            'email' => ['email', 'required', 'unique:users,email'],
-            'password' => ['required', 'min:6']
-        ]);
+        
         User::create([
+            'name' => request('name'),
             'username' => request('username'),
             'email' => request('email'),
             'password' => bcrypt(request('password')),
